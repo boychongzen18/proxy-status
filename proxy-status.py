71330b5fc45c5322d4ac0b5b6a32d2facc545320
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2019-07-25 23:38:27 +0700

    Add files via upload

---
+++ proxy-status.py
@@ -0,0 +1,23 @@
+import requests
+
+# Tools Priv8 Tools Proxy Status
+# Output proxy.txt 
+# Tested : Windows & Termux Working
+# eMAIL  : hackingforlive@yahoo.com
+# Regard : Boychongzen aka Xroot
+
+
+print ('''\033[1m\033[93m[\033[91m!\033[93m] Tools Priv8 Proxy Status [ SALE ] \033[1;37m
+Welcombek My Tools \n \033[1;31m
+  _____         _                                 
+ |_   _|__  ___| |___                             
+   | |/ _ \/ _ \ (_-<                             
+  _|_|\___/\___/_/__/    ___ _        _           
+ | _ \_ _ _____ ___  _  / __| |_ __ _| |_ _  _ ___
+ |  _/ '_/ _ \ \ / || | \__ \  _/ _` |  _| || (_-<
+ |_| |_| \___/_\_\\_, | |___/\__\__,_|\__|\_,_/__/
+                  |__/                                                        
+                             
+           \033[1;34mAuthor \033[91m:\033[37m Boychongzen aka Xroot                                  
+\033[1;34mMy \033[91m:\033[37mFacebook\033[91m:\033[37m https://web.facebook.com/xroot.xroot.7 > Xroot  \n \033[1;34m    
+ ''')
